7772564cdaa4fab988909edbc94f0a0061a7fcee
nopparatgroupthailand-ship-it <nopparatgroupthailand@gmail.com>
2026-05-10 10:49:07 +0700

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -23,7 +23,7 @@ console.log("KEY:", process.env.GEMINI_API_KEY);
 
         const model =
             genAI.getGenerativeModel({
-                model: "gemini-1.5-flash"
+                model: "gemini-2.0-flash"
             });
 
         const prompt = `
